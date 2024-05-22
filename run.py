4fa23ca0dc5c36e33a71c3aa76f5285186c50896
Rich Ricot <richricot@gmail.com>
2024-05-22 19:49:34 +0000

    Add About page & core content; fix navbar Home link

--- run.py
+++ run.py
@@ -10,6 +10,11 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
 @app.route("/register")
 def register():
     return render_template("register.html")
